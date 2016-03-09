d9581e236775c346c1b0c586fbbc37a564a54f8b
Zach Ballard <zbginji@gmail.com>
2016-03-09 14:22:05 -0600

    basic structure of program.cs

--- UnitTesting/Program.cs
+++ UnitTesting/Program.cs
@@ -10,6 +10,27 @@ namespace UnitTesting
     {
         static void Main(string[] args)
         {
+            var bob = new Bob();
+            var againString = "";
+
+            Console.WriteLine("I'd like to introduce you to Bob. He is a rather lackadaisical youth.");
+            Console.WriteLine("\nYou can talk to him. Just don't expect it to be particularly productive.");
+
+            while (true)
+            {
+
+                Console.WriteLine($"\nGo ahead and say something {againString}to Bob! > or type (exit) <");
+                var remark = Console.ReadLine();
+
+                if (remark == "exit")
+                {
+                    break;
+                }
+
+                bob.hey(remark);
+
+                againString = "else ";
+            }
         }
     }
 }
